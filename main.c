7fab25ce963900ec4de140d0c297362a182f7ef5
yehuthi <yehuthith@gmail.com>
2024-01-11 13:57:53 +0200

    improve CLI

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 #include <unistd.h>
 #include <libinput.h>
 #include <libudev.h>
 #include <fcntl.h>
 #include <poll.h>
+
+#define FATAL(...) {                                                                  \
+	fprintf(stderr, isatty(STDERR_FILENO) ? "\x1b[;31mError:\x1b[;39m " : "Error: "); \
+	fprintf(stderr, __VA_ARGS__);                                                     \
+	exit(1);                                                                          \
+}
+
+#define STRINGIFY(a) #a
+#define TO_STRING(a) STRINGIFY(a)
 
 static int open_restricted(const char *path, int flags, void *user_data) {
 	int fd = open(path, flags);
@@ -16,18 +26,35 @@ static void close_restricted(int fd, void *user_data) {
 }
 
 int main(int argc, char *argv[]) {
+	if (argc >= 2 && strcmp(argv[1],"--help") == 0) {
+		const char* cmd_head = argv[0];
+		printf("Usage:\n");
+		printf("  %s\n      Prints the mode (\"tablet\" or \"laptop\") to stdout when it changes.\n", cmd_head);
+		printf("  %s <toggle-cmd>\n      Runs <toggle-cmd> in a shell whenever the mode changes.\n", cmd_head);
+		printf("  %s <tablet-cmd> <laptop-cmd>\n      Runs <tablet-cmd> in a shell when the mode changes to tablet, and <laptop-cmd> when the mode changes to laptop.\n", cmd_head);
+		printf("  %s --help\n    Prints out this help\n", cmd_head);
+#ifdef LINUXFLIP_VERSION
+		printf("  %s --version\n    Prints out the version (%s)\n", cmd_head, TO_STRING(LINUXFLIP_VERSION));
+#endif
+		return 0;
+	}
+
+#ifdef LINUXFLIP_VERSION
+	if (argc >= 2 && strcmp(argv[1],"--version") == 0) {
+		printf("linuxflip %s\n", TO_STRING(LINUXFLIP_VERSION));
+		return 0;
+	}
+#endif
+
+	if (argc > 3)
+		FATAL("excess arguments (%d, max of 2) were supplied (did you forget to quote the arguments?)\n", argc - 1);
+
 	char* cmd_tablet = "echo tablet";
 	char* cmd_laptop = "echo laptop";
+	if (argc >= 2) cmd_tablet = cmd_laptop = argv[1];
+	if (argc >= 3) cmd_laptop = argv[2];
 
-	if (argc >= 2)
-		cmd_tablet = cmd_laptop = argv[1];
-	if (argc >= 3)
-		cmd_laptop = argv[2];
-
-	if (geteuid()) {
-		fprintf(stderr, "Error: this utility requires root privileges (did you forget `sudo`?)\n");
-		return 1;
-	}
+	if (geteuid()) FATAL("this utility requires root privileges (did you forget `sudo`?)\n");
 
 	const static struct libinput_interface file_open_close = {
 		.open_restricted = open_restricted,
